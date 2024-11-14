df566d3c35d1621b1d1a687b4b3b7652e5e9c25d
GiovanniTammaro9 <gio_play@live.it>
2024-11-14 15:21:23 +0100

    aggiunti chilometri all'auto

--- classi.js
+++ classi.js
@@ -2,18 +2,32 @@ class Automobile {
     marca =  ""
     modello = ""
     anno = 0
+    
 
-    constructor(marca, modello, anno) {
-        this.marca = marca 
-        this.modello = modello
-        this.anno = anno 
+    constructor(marca, modello, anno, chilometraggio) {
+        this.marca = marca;
+        this.modello = modello;
+        this.anno = anno;
+        this.chilometraggio = chilometraggio;
 
+    }
+
+    aggiungiChilometri(km) {
+        if(km > 0) {
+            this.chilometraggio += km;
+            console.log(`Aggiunti ${km} chilometri. chilometraggio attuale: ${this.chilometraggio} km.`);
+        }else {
+            console.log("inserire un valore positivo di kilometri");
+        }
     }
     
     descrizione() {
-        return "quest'automobile è un utilitaria , comoda ed affidabile";
+        return `marca: ${this.marca}, modello: ${this.modello}, anno: ${this.anno}, chilometraggio: ${this.chilometraggio} km`;
     }
 }
 
-let fiat = new Automobile("fiat", "bravo", 2008);
+let fiat = new Automobile("fiat", "bravo", 2008, 75.000);
+console.log(fiat.descrizione());
+
+fiat.aggiungiChilometri(400);
 console.log(fiat.descrizione());
