79ca2fbd0c5011ac21c4eeddab5be29cc143bbdc
GiovanniTammaro9 <gio_play@live.it>
2024-11-14 15:58:51 +0100

    aggiunta classe elettrica con extends e aumentata autonomia

--- classi.js
+++ classi.js
@@ -12,6 +12,8 @@ class Automobile {
 
     }
 
+    
+
     aggiungiChilometri(km) {
         if(km > 0) {
             this.chilometraggio += km;
@@ -22,12 +24,45 @@ class Automobile {
     }
     
     descrizione() {
-        return `marca: ${this.marca}, modello: ${this.modello}, anno: ${this.anno}, chilometraggio: ${this.chilometraggio} km`;
+        return `marca: ${this.marca}, modello: ${this.modello}, anno: ${this.anno}, chilometraggio: ${this.chilometraggio} km, autonomia: ${this.autonomia} km`;
     }
+
+    ricarica(km) {
+        if(km > 0) {
+            this.autonomia += km;
+            console.log(`Aggiunti ${km} chilometri. autonomia attuale: ${this.autonomia} km.`);
+        }else {
+            console.log("inserire un valore positivo di kilometri");
+        }
+    }
+
+    
 }
 
-let fiat = new Automobile("fiat", "bravo", 2008, 75.000);
+class elettrica extends Automobile {
+    autonomia = ""
+
+    constructor(marca, modello, anno, chilometraggio, autonomia) {
+        super(marca, modello, anno, chilometraggio);
+        this.autonomia = autonomia;
+    }
+
+    descrizione() {
+        return `${super.descrizione()}, autonomia: ${this.autonomia}km`;
+    }
+
+    
+}
+
+let fiat = new Automobile("fiat", "bravo", 2008, 75.000, 200);
 console.log(fiat.descrizione());
 
 fiat.aggiungiChilometri(400);
 console.log(fiat.descrizione());
+
+const autoElettrica = new elettrica("fiat", "bravo", 2008, 75.000, 200);
+console.log(autoElettrica.descrizione());
+
+autoElettrica.ricarica(200);
+console.log(autoElettrica.descrizione());
+
